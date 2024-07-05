cd26411caceaba727c4baeb99892d5283c535282
RoshanDissanayake <37024896+RoshanDissanayake@users.noreply.github.com>
2024-07-04 18:02:32 -0600

    Add files via upload

--- app5.py
+++ app5.py
@@ -148,4 +148,4 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',port=7777)
